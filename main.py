dc592f64120aeb6abc5f008d3c1264c2461aeaba
cristiane.lemos <cristianelemos@gmail.com>
2021-11-02 16:22:02 -0300

    altera main para pegar nome da proposicao e cria o arquivo csv

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ soup = BeautifulSoup(page.content, 'html.parser')
 trs = soup.find_all('tr', class_=['even', 'odd'])
 
 for tr in trs:
-    full_url = url + tr.find('a').attrs['href'].replace('../../', '')
-    data = requests.get(full_url)
+    name = tr.find_all('td')[-1].text.split(' - ')[0]
+    name = name.replace(' Nº ', '_')
+    name = name.replace('/', '_')
+    name = name.replace(' ', '')
 
-    file_name = full_url.split('/')[-1]
-    print(file_name)
-    with open('files/' + file_name, 'wb') as file:
-        file.write(data.content)
+    if (name.startswith('PL') or name.startswith('PEC')):
+        print(name)
+        full_url = url + tr.find('a').attrs['href'].replace('../../', '')
+        data = requests.get(full_url)
+        file_name = full_url.split('/')[-1]
+        file_name = name + '_' + file_name
+        print(file_name)
+        with open('files/' + file_name, 'wb') as file:
+            file.write(data.content)
