b0a22c1ff2743b9b5dddeb30c757fe5938485dfe
cristiane.lemos <cristianelemos@gmail.com>
2021-11-03 20:42:38 -0300

    adiciona get_dados_votacao

--- crawling.py
+++ crawling.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_page(url):
     url = 'https:' + url
     page = requests.get(url)
@@ -11,19 +12,25 @@ def get_page(url):
     url = 'https://forms.camara.leg.br/' + soup.find('a')['href']
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    url = soup.find('a', attrs={'class':'enquete-descricao__link'})['href']
+    url = soup.find('a', attrs={'class': 'enquete-descricao__link'})['href']
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    titulo = soup.find('h1', attrs={'class':'g-artigo__titulo'})
+    titulo = soup.find('h1', attrs={'class': 'g-artigo__titulo'})
     print(titulo)
+
+
+def get_dados_votacao(url):
+    print(url)
+
 
 def post_page():
     base_url = 'https://www.camara.leg.br/internet/votacao/default.asp'
 
     post_data = {'OutroMes': '01/8/2021'}
-    page = requests.post(base_url, data = post_data)
+    page = requests.post(base_url, data=post_data)
     soup = BeautifulSoup(page.content, 'html.parser')
     lis = soup.find_all('li', attrs={'class': None})
+    pegou_proposta = False
 
     for li in lis:
         if li.find('a') != None:
@@ -31,8 +38,15 @@ def post_page():
             text = li.find('a').text.strip()
 
             if text.startswith('PL') or text.startswith('PEC') or text.startswith('PLP'):
+                pegou_proposta = True
                 print('-----' * 10)
                 print(url)
                 get_page(url)
+            if pegou_proposta and text.startswith('Relação de votantes por UF'):
+                pegou_proposta = False
+                print('++++' * 10)
+                get_dados_votacao(
+                    'https://www.camara.leg.br/internet/votacao/' + url)
+
 
 post_page()
