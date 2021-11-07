bedaee55c3ad2801899d2240d0f3aff960c95e53
andrendarcie <andrendarcie@gmail.com>
2021-11-07 12:22:12 -0300

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from Artigo import Artigo
+import csv
 
 def get_article(url):
     url = 'https:' + url
@@ -16,8 +17,20 @@ def get_article(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
+    if soup != None:
+        return extrai_dados_do_artigo(soup)
+
+    return ''
+
+def extrai_dados_do_artigo(soup):
     titulo = soup.find('h1', attrs={'class': 'g-artigo__titulo'}).text
-    sub_titulo = soup.find('p', attrs={'class': 'g-artigo__descricao'}).text
+    print('-->', titulo)
+
+    sub_titulo = ''
+    sub_titulo_element = soup.find('p', attrs={'class': 'g-artigo__descricao'})
+    if sub_titulo_element != None:
+        sub_titulo = sub_titulo_element.text
+
     data_hora = soup.find('p', attrs={'class': 'g-artigo__data-hora'}).text
 
     div = soup.find('div', attrs={'class': 'js-article-read-more'})
@@ -25,9 +38,9 @@ def get_article(url):
     paragrafos = div.find_all('p', attrs={'class': None})
     
     for p in paragrafos:
-        resultado += p.text.strip()
+        resultado += p.text.strip().replace('\n', '')
 
-    return str(Artigo(titulo, sub_titulo, data_hora, resultado, 0))
+    return Artigo(titulo, sub_titulo, data_hora, resultado, 0)
 
 
 def get_dados_votacao(url):
@@ -36,8 +49,8 @@ def get_dados_votacao(url):
     soup = BeautifulSoup(page.content, 'html.parser')
     #print(f'Pagina voto: {url}')
 
-    proposicao = soup.find('strong').text
-    print(f'\nProposição: {proposicao}')
+    titulo = " ".join(soup.find_all('p')[2].text.split())
+    print('Titulo: ', titulo)
 
     div = soup.find('div', attrs={'id': 'corpoVotacao'})
     inicio_votacao = div.find(
@@ -64,8 +77,8 @@ def get_dados_votacao(url):
             lista_voto = lista_voto + " " + estado + " " + voto + "\n"
         voto = ""
 
-    print(f'\nVotos: {lista_voto}')
-
+    #print(f'\nVotos: {lista_voto}')
+    return titulo
 
 def post_page():
     base_url = 'https://www.camara.leg.br/internet/votacao/default.asp'
@@ -73,8 +86,14 @@ def post_page():
     post_data = {'OutroMes': '01/11/2021'}
     page = requests.post(base_url, data=post_data)
     soup = BeautifulSoup(page.content, 'html.parser')
-    lis = soup.find_all('li', attrs={'class': None})
-    pegou_proposta = False
+
+    #with open("myfile.html", "w") as file:
+    #    file.write(page.content)
+
+    lis = soup.find_all('li', attrs={ 'class': None })
+    pegou_artigo = False
+    artigos = []
+    titulos = []
 
     for li in lis:
         if li.find('a') != None:
@@ -82,14 +101,30 @@ def post_page():
             text = li.find('a').text.strip()
 
             if text.startswith('PL') or text.startswith('PEC') or text.startswith('PLP'):
-                article = get_article(url)
                 print('----' * 10)
-                print(article)
-                pegou_proposta = True
+                artigo = get_article(url)
 
-            if pegou_proposta and text.startswith('Relação de votantes por UF'):
-                pegou_proposta = False
-                #get_dados_votacao(url)
+                if artigo == '':
+                    pegou_artigo = False
+                    print('Artigo não encontrado')
+                else:
+                    pegou_artigo = True
+
+            if pegou_artigo and text.startswith('Relação de votantes por UF'):
+                pegou_artigo = False
+                artigo.titulo = get_dados_votacao(url)
+                artigos.append(artigo)
+    
+    # open the file in the write mode
+    with open('votacao.csv', 'w', encoding='UTF8', newline='') as file:
+        # create the csv writer
+        writer = csv.writer(file)
+
+        writer.writerow(['titulo', 'sub-titulo'])
+
+        for artigo in artigos:
+            # write a row to the csv file
+            writer.writerow([str(artigo.titulo), artigo.sub_titulo])
 
 
 post_page()
