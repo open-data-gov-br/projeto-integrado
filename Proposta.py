64106254b0e0c6abc2867c94e7a393dfecd99c6e
andrendarcie <andrendarcie@gmail.com>
2021-11-11 22:14:00 -0300

    Create Proposta.py

---
+++ Proposta.py
@@ -0,0 +1,12 @@
+from typing import List
+from Voto import Voto
+
+class Proposta:
+    def __init__(self, id, titulo, sub_titulo, data_hora, paragrafos, quantidade_de_votos_publicos, votacao):
+        self.id = id
+        self.titulo = titulo.strip().replace('\n','')
+        self.sub_titulo = sub_titulo.strip().replace('\n','')
+        self.data_hora = data_hora.strip()
+        self.paragrafos = paragrafos.strip().replace('\n', '')
+        self.quantidade_de_votos_publicos = quantidade_de_votos_publicos
+        self.votacao:List[Voto] = votacao
